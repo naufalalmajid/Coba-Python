1770854d3e2a69e0d7416995d551bbb6662c43d8
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-01-12 08:38:09 +0700

    Create testsort.py

---
+++ testsort.py
@@ -0,0 +1,9 @@
+x = [12, 10, 11, 34, 56, 90]
+print("====================")
+print("data sebelum sorting")
+print(x)
+print("====================")
+print("data setelah sorting")
+y = x.copy()
+y.sort()
+print(y)
